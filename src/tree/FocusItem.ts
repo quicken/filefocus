9543b57273765d29f7875cb22b90312589305102
marcel <mscherzer@gmail.com>
2023-12-09 12:30:09 +1000

    comment GroupItem and FocusItem. Refactor annotation of the GroupItem label for clarity.

--- src/tree/FocusItem.ts
+++ src/tree/FocusItem.ts
@@ -1,7 +1,24 @@
 import * as vscode from "vscode";
 
+/**
+ * Represent a FileFocus folder or file resource that is rendered inside of a
+ * vscode TreeViewComponent.
+ *
+ * Note: In FileFocus a folder is an actual resources like a directory while a Group is a logical (virtual)
+ * folder for arbitraily organising actual resources.
+ *
+ */
 export class FocusItem extends vscode.TreeItem {
   objtype = "FocusItem";
+  /**
+   *
+   * @param label The label for the node that is rendered in the TreeView
+   * @param type The vscode FileType of this resource. (Lets vscode decide what icon to render for a file based on vscodes rules/themes.)
+   * @param uri The vscode.URI that this node should point to. Typically this is a location in the workspace.
+   * @param isRootItem True if this item is directly in the root of a FileFocus Group.
+   * @param groupId The ID of the group that contains this item.
+   * @param collapsibleState See vscode.TreeItemCollapsibleState.
+   */
   constructor(
     public readonly label: string,
     public readonly type: vscode.FileType,
--- src/tree/GroupItem.ts
+++ src/tree/GroupItem.ts
@@ -1,8 +1,24 @@
 import * as vscode from "vscode";
 
+/**
+ * Represent a FileFocus Group that is rendered inside of a
+ * vscode TreeViewComponent.
+ *
+ * Note: In FileFocus a folder is an actual resources like a directory while a Group is a logical (virtual)
+ * folder for arbitraily organising actual resources.
+ *
+ */
 export class GroupItem extends vscode.TreeItem {
   objtype = "GroupItem";
 
+  /**
+   *
+   * @param label The label for the node that is rendered in the TreeView
+   * @param groupId The ID of the group that this node represents.
+   * @param collapsibleState See vscode.TreeItemCollapsibleState.
+   * @param isFavourite Set to true if this item should be marked as a favourite.
+   * @param isReadOnly Set to true to prevent editing this group node.
+   */
   constructor(
     public readonly label: string,
     public readonly groupId: string,
@@ -12,10 +28,13 @@ export class GroupItem extends vscode.TreeItem {
   ) {
     super(label, collapsibleState);
     this.contextValue = isReadOnly ? "GroupItemReadOnly" : "GroupItem";
-    this.label = this.isFavourite
-      ? `⭐ ${this.label}`
-      : this.isReadOnly
-      ? `🔹 ${this.label}`
-      : this.label;
+    this.label = this.annotateLabel(label, this.isFavourite);
+  }
+
+  private annotateLabel(label: string, isFavourite: boolean) {
+    if (isFavourite) {
+      return `⭐ ${label}`;
+    }
+    return this.isReadOnly ? `🔹 ${label}` : label;
   }
 }
