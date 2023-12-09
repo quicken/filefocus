2edd8d81a6d2f6024906f3afd8640f9e204ab0e9
marcel <mscherzer@gmail.com>
2023-12-09 12:00:01 +1000

    Comment the TreeItemBuilder class.

--- src/tree/TreeItemBuilder.ts
+++ src/tree/TreeItemBuilder.ts
@@ -4,7 +4,26 @@ import { Group } from "../Group";
 import { FocusItem } from "../tree/FocusItem";
 import { GroupItem } from "../tree/GroupItem";
 
+/**
+ * The TreeItemBuilder class is helper class used by the FileFocusTreeProvider.
+ *
+ * Specifically, this class implement methods specifically related to
+ * rendering file focus data with the vscode TreeView component.
+ *
+ * However, the class does not implement interfaces specified by vscode for working
+ * with the TreeView, those implementation are inside the FileFocusTreeProvider class.
+ *
+ */
 export class TreeItemBuilder {
+  /**
+   * Converts the resources inside a Group into FocusItems (TreeViewItems) in the order
+   * in which they should be rendered inside of an expanded TreeView node.
+   * @param group The resource group which should be converted to FocusItems.
+   * @param sortkey How the items should be sorted (order in which items should be rendered) path:sorted by the full path,
+   * basename: only sort based on the file/folder name. This way all items with the same name will be sorted together regardless
+   * of location.
+   * @returns A sorted list of FocusItems (TreeViewItems) that can be rendered by a vscode TreeView.
+   */
   public async getResourceForGroup(
     group: Group,
     sortkey: "path" | "basename"
@@ -50,6 +69,18 @@ export class TreeItemBuilder {
     return out;
   }
 
+  /**
+   * Create new TreeViewItem for representing a Folder inside a vscode TreeView.
+   *
+   * Note: In FileFocus a folder is an actual resources like a directory while a Group is a logical (virtual)
+   * folder for arbitraily organising actual resources.
+   *
+   * @param label The label rendered for the Item in the tree view.
+   * @param uri The URI the item points to.
+   * @param isRootItem True if this item is directly in the root of a Group.
+   * @param groupId The id of the Group that this item is owned by.
+   * @returns A TreeViewItem that represent a Folder.
+   */
   public createFolderItem(
     label: string,
     uri: vscode.Uri,
@@ -70,6 +101,18 @@ export class TreeItemBuilder {
     return folderItem;
   }
 
+  /**
+   * Returns TreeViewItem for rendering multiple FileFocus Groups inside a vscode TreeView.
+   *
+   * In other words this method renders the ROOT of the FileFocus TreeView.
+   *
+   * Note: In FileFocus a folder is an actual resources like a directory while a Group is a logical (virtual)
+   * folder for arbitraily organising actual resources.
+   *
+   * @param groups The set of groups that should be rendered.
+   * @param pinnedGroupId The ID of the group that should be rendered as pinned.
+   * @returns An array of TreeViewItem that represent all groups to render in a TreeView.
+   */
   public async getGroupItem(
     groups: Map<string, Group>,
     pinnedGroupId: string
@@ -84,6 +127,19 @@ export class TreeItemBuilder {
     return out;
   }
 
+  /**
+   * Create new TreeViewItem for representing an unknown/missing resource inside a vscode TreeView.
+   *
+   * An unkown item is typically a resource that can not be located inside a workspace. This can happen
+   * when syncing FileFocus settings between computers where the source code between the two machines
+   * is different such as being on a different code branch.
+   *
+   * @param label The label rendered for the Item in the tree view.
+   * @param uri The URI the item points to.
+   * @param isRootItem True if this item is directly in the root of a Group.
+   * @param groupId The id of the Group that this item is owned by.
+   * @returns A TreeViewItem that represent a unknown/missing resource.
+   */
   public createUnknownItem(
     label: string,
     uri: vscode.Uri,
@@ -117,6 +173,15 @@ export class TreeItemBuilder {
     return groupItem;
   }
 
+  /**
+   * Create new TreeViewItem for representing a file inside a vscode TreeView.
+   *
+   * @param label The label rendered for the Item in the tree view.
+   * @param uri The URI the item points to.
+   * @param isRootItem True if this item is directly in the root of a Group.
+   * @param groupId The id of the Group that this item is owned by.
+   * @returns A TreeViewItem that represent a unknown/missing resource.
+   */
   public createFileItem(
     label: string,
     uri: vscode.Uri,
