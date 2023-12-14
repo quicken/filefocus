eff6c87e46a3625b4d4529cdf4fac8067c62838f
marcel <marcel@cfavatar.com>
2023-12-14 22:31:54 +1000

    add a method that searches all workspaces by a set of glob patterns. The search includes excluded files. Introduce third party package minimatcb

--- src/FileFacade.ts
+++ src/FileFacade.ts
@@ -1,8 +1,9 @@
 import * as vscode from "vscode";
-import { workspace } from "vscode";
+import { workspace, Uri } from "vscode";
 import { FileManager } from "./FileManager";
 import { Group } from "./Group";
 import { FocusItem } from "./tree/FocusItem";
+import { minimatch } from "minimatch";
 
 /**
  * This class implements methods to afford users some limited capabilities to
@@ -30,6 +31,7 @@ export class FileFacade {
     });
 
     if (oldPath && newFilenName) {
+      vscode.workspace.workspaceFolders;
       const newPath = FileManager.renameFilenameInUri(oldPath, newFilenName);
       workspace.fs.rename(oldPath, newPath, { overwrite: false });
       if (focusItem.isRootItem) {
@@ -62,5 +64,42 @@ export class FileFacade {
       workspace.fs.createDirectory(newFolderUri);
       group.addResource(newFolderUri);
     }
+  }
+
+  /**
+   * Recursively search for all files and folders that match the passed in glob patterns in
+   * all workspace folders. This method also searches files that would normally be excluded by VSCode.
+   *
+   * Note: This may work slightly different to the VSCode glob matching as VSCode currently provides
+   * no direct access to its implementation. As such we rely on a third party glob package that
+   * may work slightly differently.
+   *
+   * @param baseUri The base uri from which to start the search. This must be a folder URI.
+   * @param patterns An array of GLOB patterns that define what should be matched.
+   * @returns An array of URIs that match the glob pattern.
+   */
+  static async searchAllFilesAndFolders(
+    baseUri: Uri,
+    patterns: string[]
+  ): Promise<Uri[]> {
+    const matches: Uri[] = [];
+    const listing = await workspace.fs.readDirectory(baseUri);
+    for (const [name, type] of listing) {
+      const entryUri = vscode.Uri.joinPath(baseUri, name);
+      if (type === vscode.FileType.Directory) {
+        const uris = await FileFacade.searchAllFilesAndFolders(
+          entryUri,
+          patterns
+        );
+        matches.push(...uris);
+      }
+      for (const pattern of patterns) {
+        if (minimatch(name, pattern)) {
+          matches.push(entryUri);
+          break;
+        }
+      }
+    }
+    return matches;
   }
 }
